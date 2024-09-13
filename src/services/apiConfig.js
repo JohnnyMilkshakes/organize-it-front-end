380e16f8391990d80e2c1c1a7d9a22dd32916f4b
JohnnyMilkshakes <137217953+JohnnyMilkshakes@users.noreply.github.com>
2024-09-13 16:40:07 -0400

    basic setup

---
+++ src/services/apiConfig.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const getToken = () => {
+  return new Promise((resolve) => {
+    const token = localStorage.getItem("token");
+    resolve(token ? `Bearer ${token}` : null);
+  });
+};
+
+let apiUrl;
+
+const apiUrls = {
+  production: "http://localhost:8000",
+  development: "http://localhost:8000",
+};
+
+if (window.location.hostname === "localhost") {
+  apiUrl = apiUrls.development;
+} else {
+  apiUrl = apiUrls.production;
+}
+const api = axios.create({
+  baseURL: apiUrl
+});
+
+api.interceptors.request.use(
+  async function (config) {
+    const token = await getToken();
+    if (token) {
+      config.headers["Authorization"] = token;
+    }
+    return config;
+  },
+  function (error) {
+    console.log("Request error: ", error);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
