165bc02b930b01d9ba526718b914a7a1547fb2d1
wendy-silva <silvawendy0131@gmail.com>
2024-09-17 11:47:11 -0400

    adjust css and style page

--- src/components/SignUpLogIn.jsx
+++ src/components/SignUpLogIn.jsx
@@ -50,9 +50,9 @@ const Home = () => {
                 </form>
             </div>
             <div className='welcome-section'>
-                <h1>Welcome!</h1>
+                <h1>Welcome to OrganizeIt!</h1>
                 <p>
-                    wjhefhsudfhaisfhisd.
+                    The smart way to manage your inventory. Track and stay organized with ease. Our user-friendly system helps you save time, reduce errors, and stay in control of your business. Get started with OrganizeIt and simplify your invenotry today!
                 </p>
             </div>
         </div>
