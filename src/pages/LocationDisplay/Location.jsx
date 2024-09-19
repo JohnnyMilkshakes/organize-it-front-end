a960b022e3c302e6c432022409e518922d3c0797
JohnnyMilkshakes <137217953+JohnnyMilkshakes@users.noreply.github.com>
2024-09-19 11:47:30 -0400

    changes location keyword to 'site'

--- src/pages/LocationDisplay/Location.jsx
+++ src/pages/LocationDisplay/Location.jsx
@@ -1,13 +1,27 @@
 import { useState, useEffect } from "react";
 import { getItems } from "../../services/items"; // Service to get items for a location
+import { getLocation } from "../../services/locations";
 import { useParams } from "react-router-dom";
 import ItemForm from "../../components/ItemForm/ItemForm";
 import ItemTile from "../../components/ItemTile/ItemTile";
 
 const Location = () => {
   const { locationId } = useParams();
+  const [site, setSite] = useState(null); 
   const [items, setItems] = useState([]); // Track items for selected location
   const [showForm, setShowForm] = useState(false); // Show/hide the add item form
+
+  useEffect(() => {
+    const fetchSite = async () => {
+      try {
+        const data = await getLocation(locationId);
+        setSite(data);
+      } catch (err) {
+        console.log("Failed to load locations", err);
+      }
+    };
+    fetchSite();
+  }, [locationId]);
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -23,10 +37,10 @@ const Location = () => {
 
   return (
     <div>
-      {location ? (
+      {site ? (
         <>
-          <h1>Location: {location.name}</h1>
-          <p>{location.address}</p>
+          <h1>Location: {site.name}</h1>
+          <p>{site.address}</p>
 
           <div>
 
